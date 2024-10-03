f53a208ab55ce1462eced616ad249a24aad5655d
adamsolaide76@gmail.com <adamsolaide76@gmail.com>
2024-10-03 14:48:30 +0100

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+let present = document.getElementById("present");
+function DropDown() {
+  present.classList.toggle("active");
+}
+let Talk = document.getElementsByClassName("lettalk");
+
+
+
+let Button = document.querySelector("#Menu")
+console.log(Button);
+Button (params) => {
+  
+}
+
