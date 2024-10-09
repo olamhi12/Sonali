075aec808a69762ef4a7f8de9d5cece4dc465e65
adamsolaide76@gmail.com <adamsolaide76@gmail.com>
2024-10-09 16:50:15 +0100

    update

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-// function DropDown() {
-//   let present = document.getElementById("present");
-//   present.classList.toggle("active");
-// }
-
-// function GetMe() {
-//   let Talk = document.getElementsByClassName("lettalk"); 
-//   Talk.classList.add("lettalk2")
-// }
-
-// let Button = document.querySelector("#Menu")
-// console.log(Button);
+function DropDown() {
+  let present = document.getElementById("present");
+  present.classList.toggle("active");
+}
+let input = document.querySelector("input")
+function Sub() {
+  alert(`you are ${input.value} , Welcome to Adams website`)
+  
+}
+let Frame = document.querySelector(".frame")
+function Menu() {
+  Frame.classList.toggle("act")
+  console.log(Frame);
+  
+}
+let Talk = document.querySelector(".lettalk"); 
+function GetMe() {
+  Talk.classList.add("lettalk2")
+}
+function Close() {
+  Talk.classList.remove("lettalk2")
+  console.log("hello");
+  
+}
+let Button = document.querySelector("#Menu")
+console.log(Button);
 
 
